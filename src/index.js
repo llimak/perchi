154023eb591f891839a992bce0625204d4758783
Kamil Popczyk <kam.popczyk@gmail.com>
2022-01-11 01:15:09 +0100

    Added Atbash cipher in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { CaesarCipher } = require("./../src/caesar.js");
 const { VigenereCipher } = require("./../src/vigenere.js");
+const { AtbashCipher } = require("./../src/atbash.js");
 
 module.exports = {
   CaesarCipher,
   VigenereCipher,
+  AtbashCipher,
 };
